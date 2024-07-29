82650ab0e41ba6148702ac02c966409b9a05eb6d
Abhishek Soni <abhishekkssoni0786@gmail.com>
2024-07-29 14:56:53 +0530

    29-07-2024

--- app/src/main/java/com/example/pizzaorderingapp/ui/screen/Checkout.kt
+++ app/src/main/java/com/example/pizzaorderingapp/ui/screen/Checkout.kt
@@ -3,17 +3,20 @@ package com.example.pizzaorderingapp.ui.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import java.util.zip.CheckedOutputStream
 
 @Composable
 fun CheckoutScreen(modifier: Modifier = Modifier) {
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color.Blue))
+        .background(MaterialTheme.colorScheme.background))
+
 
     }
 
