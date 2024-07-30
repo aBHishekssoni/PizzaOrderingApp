1271a8cc6fdb4f0b2d9dec5a895be61f5a906710
Abhishek Soni <abhishekkssoni0786@gmail.com>
2024-07-30 16:08:23 +0530

    30-07-2024

--- app/src/main/java/com/example/pizzaorderingapp/ui/screen/TopingSelection.kt
+++ app/src/main/java/com/example/pizzaorderingapp/ui/screen/TopingSelection.kt
@@ -1,5 +1,6 @@
 package com.example.pizzaorderingapp.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
@@ -24,6 +25,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,37 +44,56 @@ import com.example.pizzaorderingapp.dummyToppingList
 fun ToppingSelection(
     modifier: Modifier = Modifier,
     pizzaid: Int,
-    onConfirm: () -> Unit
+    onConfirm: (Set<Topping>) -> Unit ={},
 ) {
     val pizza = dummyPizzaList()[pizzaid]
+    var SelectedTopping by remember { mutableStateOf(emptySet<Topping>()) }
+
+    var selectedcheckbox by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.onBackground)
     ) {
-Column {
-    Text(text = "Select Topping",
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        modifier = Modifier.padding(8.dp)
-    )
-    Text(text = "${pizza.name} selected", modifier = Modifier.padding(8.dp))
-    Spacer(modifier = Modifier.padding(8.dp))
-}
-    LazyColumn(
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.fillMaxSize()
-    ) {
+        Column {
+            Spacer(modifier = Modifier.padding(12.dp))
+
+            Text(
+                text = "Select Topping",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                modifier = Modifier.padding(8.dp), color = MaterialTheme.colorScheme.secondary
+            )
+            Text(
+                text = "${pizza.name} selected", modifier = Modifier.padding(8.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            LazyColumn(
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
 
 
-            items(dummyToppingList()) {
-                ToppingsItem(toppings = it)
+                items(dummyToppingList()) {
+                    ToppingsItem(toppings = it){selection->
+                        if (selection.isChecked) {
+                           SelectedTopping=  SelectedTopping.plusElement(selection)
+                        } else {
+                            SelectedTopping = SelectedTopping.minusElement(selection)
+                        }
+                        Log.d("checkbox value","$SelectedTopping")
+//                        SelectedTopping.toMutableList().add(selection)
+                    }
 
+                }
+
+            }
         }
-
-    }
-        FloatingActionButton(onClick = { onConfirm },
+        FloatingActionButton(
+            onClick = { onConfirm },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(16.dp),
@@ -78,28 +102,39 @@ Column {
         ) {
             Icon(imageVector = Icons.Default.Done, contentDescription = "Done")
         }
+    }
 }
-}
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ToppingsItem(modifier: Modifier = Modifier,
-                 toppings:Topping) {
-    Card (
+fun ToppingsItem(
+    modifier: Modifier = Modifier,
+    toppings: Topping,
+    ontoppingselected: (Topping) -> Unit
+
+) {
+    var selectedcheckbox by remember { mutableStateOf(false) }
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(2.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.tertiary
         )
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(6.dp),
             verticalAlignment = Alignment.CenterVertically,
 
-        ){
-            Checkbox(checked = toppings.isChecked, onCheckedChange ={toppings.isChecked = it})
+            ) {
+            Checkbox(checked = selectedcheckbox, onCheckedChange = { Log.d("checkbox value","$it")
+            selectedcheckbox = it
+            toppings.isChecked = selectedcheckbox
+            ontoppingselected(toppings)
+
+            })
 //            Column(
 //                modifier
 //                    .height(100.dp)
@@ -110,24 +145,24 @@ fun ToppingsItem(modifier: Modifier = Modifier,
 
 //            )
 //            {
-                Text(
-                    text = toppings.title,
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .basicMarquee()
-                        .padding(10.dp)
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Text(
-                    text = "Rs ${toppings.price}",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    color = Color.Blue.copy(alpha = .6f),
-                    modifier = Modifier.padding(10.dp)
+            Text(
+                text = toppings.title,
+                style = MaterialTheme.typography.bodyLarge,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .basicMarquee()
+                    .padding(10.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "Rs ${toppings.price}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.Blue.copy(alpha = .6f),
+                modifier = Modifier.padding(10.dp)
 
-                )
+            )
 //            }
 
         }
@@ -136,13 +171,15 @@ fun ToppingsItem(modifier: Modifier = Modifier,
 
 
 }
+
 @Preview
 @Composable
 private fun ToppingsItemPreview() {
     ToppingsItem(
         toppings = Topping(
-            "Pepper",10.00,)
-    )
+            "Pepper", 10.00,
+        )
+    ){}
 
 }
 
